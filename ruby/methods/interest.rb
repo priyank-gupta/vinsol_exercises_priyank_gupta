88e2e6bd21fc3feaf450dd742805647f4af8753a
Priyank Gupta <priyank.gupta@vinsol.com>
2011-10-08 18:35:43 +0530

    method commit 3

--- ruby/methods/interest.rb
+++ ruby/methods/interest.rb
@@ -1,7 +1,7 @@
 
 class Interest
 
-	def initialize (p, t, &block)
+	def initialize (&block)
 	
 		block.call(p,t)
 	end
@@ -12,15 +12,16 @@ print "enter the principle amount : "
 p = gets.chomp.to_f
 print "enter the time : "
 t = gets.chomp.to_f
-i1 = Interest.new(p,t) do
-														|p,t|
-														simple_i = p * t * 0.1
-														compound_i = p * 0.1**t
-														if simple_i > compound_i 
-															diff = simple_i - compound_i
-														else
-															diff = compound_i - simple_i
-														end
-														print "the difference b/w simply and compoundedly calculated interest is #{diff}"
-														puts
-											end
+i1 = Interest.new(p,t) 
+		do
+			|p,t|
+			simple_i = p * t * 0.1
+			compound_i = p * 0.1**t
+			if simple_i > compound_i 
+				diff = simple_i - compound_i
+			else
+				diff = compound_i - simple_i
+			end
+			print "the difference b/w simply and compoundedly calculated interest is #{diff}"
+			puts
+		end
