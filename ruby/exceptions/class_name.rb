b5da599d194e20f3a6b16ab8f88243894792776b
Priyank Gupta <priyank.gupta@vinsol.com>
2011-10-11 15:58:15 +0530

    exceptions commit 2

--- ruby/exceptions/class_name.rb
+++ ruby/exceptions/class_name.rb
@@ -4,11 +4,11 @@ class Name
 	attr_reader :first_name , :last_name
 	
 	def initialize (first, last)
-		@first_name = first
-		@last_name = last
+		first_test first
+		last_test last
 	end
 	
-	def first_name=(first)
+	def first_test first
 		begin
 			if first == nil || first.length == 0
 				raise ("Should have first name")
@@ -16,10 +16,11 @@ class Name
 			@first_name = first.capitalize
 		rescue Exception => e
 			puts e.message
+			exit
 		end
 	end
 	
-	def last_name=(last)
+	def last_test last
 		begin
 			if last == nil || last.length == 0
 				raise ("Should have last name")
@@ -27,7 +28,16 @@ class Name
 			@last_name = last
 		rescue Exception => e
 			puts e.message
+			exit
 		end
+	end
+	
+	def first_name=(first)
+		first_test first
+	end
+	
+	def last_name=(last)
+		last_test last
 	end
 	
 	def full_name
@@ -36,9 +46,9 @@ class Name
 
 end
 
-naam1 = Name.new('priyank', 'gupta')
+naam = Name.new('priyank', 'gupta')
 puts naam1.full_name  
-naam1.first_name = 'shubham'  
-puts naam1.full_name 
-naam1.first_name = nil
-naam1.last_name = ""
+naam.first_name = 'shubham'  
+puts naam.full_name 
+naam.first_name = nil
+naam.last_name = ""
