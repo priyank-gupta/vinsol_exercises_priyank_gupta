084c7f495209f63f247b893d6beb6ab93a26488c
Shreya Bhatia <shreya@Shreya-Bhatias-MacBook-Pro.local>
2011-10-13 10:55:43 +0530

    Initial Commit

--- ruby/practise_exercise/xml_format.rb
+++ ruby/practise_exercise/xml_format.rb
@@ -1,49 +1,56 @@
+##### 1. Move require statements to the top
+##### 2. Check if the filename is valid and it exists
+##### 3. Use Nokogiri to create nodes, the result should be valid xml. 
+##### 4. Can you also put the output to another file instead of displaying the xml formatting on the console?
+
 
 class XML_Format
 	
 	require 'rubygems'
 	require 'nokogiri'
 	
+	##### Dont use global variable that are accessible outside the class here. You can use class variables
 	$arr = []			#global variable for final array
 	
 	def initialize f
-	
 		file = File.open(f)			#opening xml file and saving its content
 		xml_doc = Nokogiri::XML(file)
 		file.close
 		root_child = xml_doc.root().children
 		iterate root_child
 		output $arr
-		
 	end
 	
 	private				#methods are private after this
 	
-	def iterate root_child_itr			#to iterate over the nodes
+	## I like the fact that you've commented your code.
+	## But most importantly, you should comment the logic part
+	## Example - you've written 'root_child_itr.children.children[0]' explain what this is doing
 	
+	def iterate root_child_itr			#to iterate over the nodes		
+	  
+	  ## Collect root_child_itr.children in a variable since it being used twice
+	  ## Can you try using some other conditional construct other than loop and break?
 		loop do
-	
 			if root_child_itr.children.children[0] == nil
 				$arr.push root_child_itr.children
-				break
+        break
 			else
 				scan root_child_itr
-				break
-			end
-			
+        break
+			end		
 		end
 	end
 	
-	def scan root_child_scan			# to iterate over the childnodes
-
-		for i in 0...root_child_scan.length do
 	
+	def scan root_child_scan			# to iterate over the childnodes
+		for i in 0...root_child_scan.length do
 			if root_child_scan[i].text =~ /\w\s*\n/
 				iterate root_child_scan[i].children
 			end
-	
 		end			
 	end
+	
 	
 	def output arr_out			#to print the formated file
 	
