d028abd577b37dc78814294763dd77bda1b26264
Priyank Gupta <priyank.gupta@vinsol.com>
2011-10-13 13:04:57 +0530

    commit 2

--- ruby/practise_exercise/xml_format.rb
+++ ruby/practise_exercise/xml_format.rb
@@ -3,22 +3,21 @@
 ##### 3. Use Nokogiri to create nodes, the result should be valid xml. 
 ##### 4. Can you also put the output to another file instead of displaying the xml formatting on the console?
 
+require 'rubygems'
+require 'nokogiri'
 
 class XML_Format
 	
-	require 'rubygems'
-	require 'nokogiri'
-	
 	##### Dont use global variable that are accessible outside the class here. You can use class variables
-	$arr = []			#global variable for final array
 	
-	def initialize f
-		file = File.open(f)			#opening xml file and saving its content
+	def initialize file
+		
+		@@arr = []
 		xml_doc = Nokogiri::XML(file)
 		file.close
 		root_child = xml_doc.root().children
 		iterate root_child
-		output $arr
+		output @@arr
 	end
 	
 	private				#methods are private after this
@@ -31,12 +30,13 @@ class XML_Format
 	  
 	  ## Collect root_child_itr.children in a variable since it being used twice
 	  ## Can you try using some other conditional construct other than loop and break?
-		loop do
-			if root_child_itr.children.children[0] == nil
-				$arr.push root_child_itr.children
+		while true
+			temp = root_child_itr.children
+			if temp.children[0] == nil			#checking if the node is innermost as innermost node will not be having any childnode
+				@@arr.push temp
         break
 			else
-				scan root_child_itr
+				scan root_child_itr		# iterate over the childnodes if it is not the parent of the innermost node
         break
 			end		
 		end
@@ -45,7 +45,7 @@ class XML_Format
 	
 	def scan root_child_scan			# to iterate over the childnodes
 		for i in 0...root_child_scan.length do
-			if root_child_scan[i].text =~ /\w\s*\n/
+			if root_child_scan[i].text =~ /\w\s*\n/		#ignoring newlines as they are also the child of node
 				iterate root_child_scan[i].children
 			end
 		end			
@@ -53,22 +53,52 @@ class XML_Format
 	
 	
 	def output arr_out			#to print the formated file
-	
-		puts "<?xml version='1.0'?>\n<people>\n"
+		
+		out_doc = File.new("output.xml", "w")
+		doc = Nokogiri::XML::DocumentFragment.parse(out_doc)
+		people = Nokogiri::XML::Node.new "people", doc
 		
 		for i in 0...arr_out.length do
-			puts "	<person>\n		<name>"
-			puts "			<first>#{arr_out[i][0]}</first>"
-			puts "			<last>#{arr_out[i][1]}</last>"
-			puts "		</name>\n	</person>"
+			
+			person = Nokogiri::XML::Node.new "person", doc
+			people.add_child(person)
+			name = Nokogiri::XML::Node.new "name", doc
+			person.add_child(name)
+			first = Nokogiri::XML::Node.new "first", doc
+			last = Nokogiri::XML::Node.new "last", doc
+			address = Nokogiri::XML::Node.new "address", doc
+			
+			for j in 0...arr_out[i].length do
+				
+				if (arr_out[i][j] && j == 0)
+					first.content = arr_out[i][j]
+					name.add_child (first)
+				elsif (arr_out[i][j] && j == 1)
+					last.content = arr_out[i][j]
+					first.add_next_sibling(last)
+				elsif (arr_out[i][j] && j == 2)
+					address.content = arr_out[i][j]
+					last.add_next_sibling(address)
+				end				
+				
+			end
 		end
 		
-		puts "</people>"
+		out_doc.puts "<?xml version='1.0'?>"
+		out_doc.puts people
+		puts "Check output.xml for output"
 	end
 	
 end
 
-puts "enter the filename you want to format (test1.xml/test2.xml/test3.xml)"
-file = gets.chomp				#asking for input of a filename from user
+begin			#exception block for checking the availability of file
+	puts "\nenter the filename you want to format (test1.xml/test2.xml/test3.xml)"
+	f = gets.chomp				#asking for input of a filename from user
+	file = File.open(f)			#opening xml file and saving its content
+rescue StandardError => e			#exception handler
+	puts "File not found... Try Again"
+	retry
+end
+	
 XML_Format.new(file)			#instantiating the XML_Foramt class
 
