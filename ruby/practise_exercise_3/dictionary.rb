701afbde7f3de2cea8cfa9b9f715b4bc47fc1e15
Priyank Gupta <priyank.gupta@vinsol.com>
2011-10-21 16:37:53 +0530

    dictionary comment commit

--- ruby/practise_exercise_3/dictionary.rb
+++ ruby/practise_exercise_3/dictionary.rb
@@ -1,17 +1,17 @@
 module WordMatch
 	def num_options
-		num_opt = gets.chomp.to_i
+		num_opt = gets.chomp.to_i								#input number of options to enter
 	end
 		
 	def wrong_word
-		word_to_find = gets.chomp
+		word_to_find = gets.chomp								# input the word to find
 	end
 	
 	def options(num)
 		opt = []
 		for i in 0...num do
 			print "#{i+1} : " 
-			opt[i] = gets.chomp
+			opt[i] = gets.chomp										#taking option strings in an array
 		end
 		opt
 	end
@@ -19,28 +19,29 @@ end
 	
 module LongestCommonSubsequence
 	
-	def lcs(wrong_word, opt)
+	def lcs(wrong_word, opt)									#method to find the longest common subsequence of two words
 		m = wrong_word.length
 		n = opt.length
-		c = Array.new(m+1){Array.new(n+1)}
+		c = Array.new(m+1){Array.new(n+1)}			#making a two dimensional array
 		for i in 0..m do
-			c[i][0] = 0
+			c[i][0] = 0														#elements of first row will be zero
 		end
 		for j in 1..n do
-			c[0][j] = 0
+			c[0][j] = 0														#elements of first column will be zero
 		end
 		for i in 1..m do
 			for j in 1..n do
-				if wrong_word[i-1] == opt[j-1]
-					c[i][j] = c[i][j-1] + 1
+				if wrong_word[i-1] == opt[j-1]			#if the alphabets match
+					c[i][j] = c[i][j-1] + 1						#add 1 to the number that is in the previous column and store that in the current field
 				else
-					c[i][j] = c[i][j-1] > c[i-1][j] ? c[i][j-1] : c[i-1][j]
+					c[i][j] = c[i][j-1] > c[i-1][j] ? c[i][j-1] : c[i-1][j]				#store the number which is larger b/w previous column and previous row in the current field
 				end
 			end
 		end
 	
-		longest_subseq_length = c.max.max
+		longest_subseq_length = c.max.max																		#finding the length of longest subsequence
 		str = ""
+		# backtracking the matched characters in the array
 		for i in 0..m do
 			for j in 0..n do
 				if c[m-i][n-j] == longest_subseq_length && wrong_word[m-i-1] == opt[n-j-1] && longest_subseq_length > 0
@@ -49,10 +50,10 @@ module LongestCommonSubsequence
 				end
 			end
 		end
-		str.reverse
+		str.reverse						#returning the longest subsequence string
 	end
 	
-	def str_with_lcs(opts, subseq_length_arr)
+	def str_with_lcs(opts, subseq_length_arr)								#finding the string with longest subsequence in options array
 		max_length = subseq_length_arr.max
 		arr = []
 		subseq_length_arr.each_with_index do |val, index|
@@ -72,9 +73,9 @@ class  Dictionary
 	def find_correct_word(wrong_word, opts)
 		subsequence_arr = []
 		opts.each_with_index do |val, index|
-			subsequence_arr[index] = lcs(wrong_word, val).length
+			subsequence_arr[index] = lcs(wrong_word, val).length							#storing the length of the longest common subsequences of each option in an array in the same index as they are
 		end
-		answer_arr = str_with_lcs(opts, subsequence_arr)
+		answer_arr = str_with_lcs(opts, subsequence_arr)										#array with options that has the longest common subsequence among others
 	end
 
 end
@@ -89,7 +90,7 @@ begin
 	opts = d.options(num)
 	answer = d.find_correct_word(wrong_word, opts)
 	puts "Answer :" 
-	answer.each {|val| puts "	#{val}"}
+	answer.each {|val| puts "	#{val}"}																		#printing each element separately by extracting them from array
 	print "Do you want to continue (y/Y for yes): "
 	choice = gets.chomp
 end while (choice == 'y' || choice == 'Y')
