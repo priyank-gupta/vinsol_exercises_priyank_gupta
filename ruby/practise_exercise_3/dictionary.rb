0263950714391c51845dc694115697c319ca5a2d
Shreya Bhatia <shreya@Shreya-Bhatias-MacBook-Pro.local>
2011-10-21 18:05:50 +0530

    Good job in terms for learning from previous exercise - modularization of code, and code quality

--- ruby/practise_exercise_3/dictionary.rb
+++ ruby/practise_exercise_3/dictionary.rb
@@ -1,3 +1,4 @@
+#### Godd Job!
 module WordMatch
 	def num_options
 		num_opt = gets.chomp.to_i								#input number of options to enter
@@ -23,12 +24,15 @@ module LongestCommonSubsequence
 		m = wrong_word.length
 		n = opt.length
 		c = Array.new(m+1){Array.new(n+1)}			#making a two dimensional array
+	
 		for i in 0..m do
 			c[i][0] = 0														#elements of first row will be zero
 		end
+	
 		for j in 1..n do
 			c[0][j] = 0														#elements of first column will be zero
 		end
+	
 		for i in 1..m do
 			for j in 1..n do
 				if wrong_word[i-1] == opt[j-1]			#if the alphabets match
@@ -39,28 +43,14 @@ module LongestCommonSubsequence
 			end
 		end
 	
-		longest_subseq_length = c.max.max																		#finding the length of longest subsequence
-		str = ""
-		# backtracking the matched characters in the array
-		for i in 0..m do
-			for j in 0..n do
-				if c[m-i][n-j] == longest_subseq_length && wrong_word[m-i-1] == opt[n-j-1] && longest_subseq_length > 0
-					str += wrong_word[m-i-1]
-					longest_subseq_length -= 1
-				end
-			end
-		end
-		str.reverse						#returning the longest subsequence string
+		c.max.max																		#finding the length of longest subsequence
 	end
 	
 	def str_with_lcs(opts, subseq_length_arr)								#finding the string with longest subsequence in options array
 		max_length = subseq_length_arr.max
 		arr = []
-		subseq_length_arr.each_with_index do |val, index|
-			if val == max_length
-				arr.push opts[index]
-			end
-		end
+		### Use collect for below functinality
+		subseq_length_arr.each_with_index { |val, index|	arr.push opts[index] if val == max_length }
 		arr
 	end
 end			
@@ -73,9 +63,9 @@ class  Dictionary
 	def find_correct_word(wrong_word, opts)
 		subsequence_arr = []
 		opts.each_with_index do |val, index|
-			subsequence_arr[index] = lcs(wrong_word, val).length							#storing the length of the longest common subsequences of each option in an array in the same index as they are
+			subsequence_arr[index] = lcs(wrong_word, val)							         #storing the length of the longest common subsequences of each option in an array in the same index as they are
 		end
-		answer_arr = str_with_lcs(opts, subsequence_arr)										#array with options that has the longest common subsequence among others
+		str_with_lcs(opts, subsequence_arr)										#array with options that has the longest common subsequence among others
 	end
 
 end
