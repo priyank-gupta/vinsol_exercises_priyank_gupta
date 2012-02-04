9527cc43b34b38368b3d1bfb20600a13743f4271
Priyank Gupta <priyank.gupta@vinsol.com>
2012-02-04 18:13:22 +0530

    small change

--- shell_scripting/company_name_change/script.sh
+++ shell_scripting/company_name_change/script.sh
@@ -2,24 +2,24 @@
 
 if [ -e "./$1" ] && [ -r "./$1" ] && [ -w "./$1" ]; then
 
-	exec 10<&0
-	exec < $1
+  exec 10<&0
+  exec < $1
 
-	while read LINE; do
+  while read LINE; do
 
-		if [[ ! $LINE =~ (http://www\..+\.com) && $LINE =~ ^(.+)(www\..+\.com)$ ]]; then
-			echo "${BASH_REMATCH[1]}http://${BASH_REMATCH[2]}"
-		else
-		  echo $LINE
-	  fi
+    if [[ ! $LINE =~ (http://www\..+\.com) && $LINE =~ ^(.+)(www\..+\.com)$ ]]; then
+      echo "${BASH_REMATCH[1]}http://${BASH_REMATCH[2]}"
+    else
+      echo $LINE
+    fi
 
-	done < $1 > "$1.t"
+  done < $1 > "$1.t"
 
   mv $1{.t,}
   
-	exec 0<&10 10<&-
+  exec 0<&10 10<&-
 
 else
-	echo "File $1 doesn't exist or don't have appropriate permissions for read/write."
+  echo "File $1 doesn't exist or don't have appropriate permissions for read/write."
 fi
 
